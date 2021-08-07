3bf0838c1820857fbd911c21d0d796e67e4370b5
Ata-92 <atakal92@gmail.com>
2021-08-07 17:32:00 +0300

    Add model to admin panel

--- src/student_api/views.py
+++ src/student_api/views.py
@@ -1,10 +1,8 @@
 
 from django.shortcuts import render, HttpResponse
-
 from .models import Student
 
 # Create your views here.
-
 
 def home(request):
     return HttpResponse('<h1>API Page</h1>')
